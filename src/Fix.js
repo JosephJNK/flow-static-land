851053bedc4a828a5712a0df3f93aa5b478beebc
Joseph Junker <joseph.jnk@gmail.com>
2017-07-08 20:36:03 -0700

    adds functor fixpoint

---
+++ src/Fix.js
@@ -0,0 +1,15 @@
+// @flow
+import type { HKT } from "./HKT";
+
+export type Fix<F> = In<F>;
+
+export class In<F> {
+  term: HKT<F, Fix<F>>;
+  constructor(term: HKT<F, Fix<F>>) {
+    this.term = term;
+  };
+};
+
+export function out<F>(term: Fix<F>) : HKT<F, Fix<F>> {
+  return term.term;
+}
